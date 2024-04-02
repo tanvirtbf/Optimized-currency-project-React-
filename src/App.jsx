338fe2de0435ab46bc68e4b4a58bc49c954cb618
tanvirtbf <tanver018765@gmail.com>
2024-04-02 15:03:31 +0600

    state setup in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./App.css";
-
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 function App() {
+  const [amount,setAmount] = useState(0)
+  const [from,setFrom] = useState("usd")
+  const [to,setTo] = useState("inr")
+  const [convertedAmount,setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  
   return (
     <>
       <h1 className="text-3xl bg-orange-500">Currency App</h1>
